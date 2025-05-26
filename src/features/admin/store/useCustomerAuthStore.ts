aea4305b3b2e3f1df97ef76d603686d15d4219ef
from-ryw <from.ryw@gmail.com>
2025-05-26 18:14:41 +0900

    feat: 프론트엔드 초기 구조 및 라우팅/레이아웃 구성

---
+++ src/features/admin/store/useCustomerAuthStore.ts
@@ -0,0 +1,8 @@
+import { create } from 'zustand';
+import type { AuthState } from '@/types/auth';
+
+export const useCustomerAuthStore = create<AuthState>((set) => ({
+  isLoggedIn: false,
+  login: () => set({ isLoggedIn: true }),
+  logout: () => set({ isLoggedIn: false }),
+}));
