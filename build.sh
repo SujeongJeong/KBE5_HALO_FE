a912b41264f1871cd10ab14f71e6472005d4b5ba
SujeongJeong <suujeong0@gmail.com>
2025-06-28 21:28:19 +0900

    refactor : build 체크 추가

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
-  
 #!/bin/sh
-cd ../
+set -e
+
+# 1. 프론트엔드 폴더로 이동
+cd ./KBE5_HALO_FE
+
+# 2. 의존성 설치 및 빌드
+npm ci
+npm run build
+
+# 3. output 디렉토리 준비
+cd ..
+rm -rf output        # 이전 output 제거 (선택)
 mkdir output
-cp -R ./KBE5_HALO_FE/* ./output
-cp -R ./output ./KBE5_HALO_FE
+
+# 4. 빌드 결과물을 output으로 복사 (build 폴더만!)
+cp -R ./KBE5_HALO_FE/build/* ./output
