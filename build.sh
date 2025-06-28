be928f70787f0451fb945de6c550d0a536fd3944
SujeongJeong <suujeong0@gmail.com>
2025-06-28 21:34:29 +0900

    refactor: build 파일 수정

--- build.sh
+++ build.sh
@@ -9,9 +9,8 @@ npm ci
 npm run build
 
 # 3. output 디렉토리 준비
-cd ..
 rm -rf output        # 이전 output 제거 (선택)
 mkdir output
 
 # 4. 빌드 결과물을 output으로 복사 (build 폴더만!)
-cp -R ./KBE5_HALO_FE/build/* ./output
+cp -R ./build/* ./output
