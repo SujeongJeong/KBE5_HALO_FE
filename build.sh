50c20b10dc76332da98658035dd630a57389767c
SujeongJeong <suujeong0@gmail.com>
2025-06-30 01:24:00 +0900

    refactor : 배포라인 초기로 수정

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-rm -rf output
+cd ../
 mkdir output
-
-# 2. copy current repo frontend (KBE5_HALO_FE) to output
-# 현재 위치가 루트라고 가정하고 ./KBE5_HALO_FE 안에 frontend 코드 있다고 가정
 cp -R ./KBE5_HALO_FE/* ./output
-
+cp -R ./output ./KBE5_HALO_FE/
